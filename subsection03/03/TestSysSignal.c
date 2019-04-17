4fc9cf6e60cdcb4ef72406da64f1bad3201b84ae
liuxueneng <liu_xueneng@163.com>
2019-04-17 17:16:54 +0800

    finish section03_03 at 20190417
    finish section03_03 at 20190417

---
+++ subsection03/03/TestSysSignal.c
@@ -0,0 +1,44 @@
+/*
+ * file:        TestSysSignal.c
+ * func:        system signal handler
+ * author:      liuxueneng@airfly
+ * date:        20190417
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+
+
+void sig_handler(int signo)
+{
+    if (signo == SIGQUIT || signo == SIGTERM) {
+        printf("Bye\n");
+        exit(0);
+    }
+    else
+        printf("ignor sig\n");
+}
+
+int main(void)
+{
+    if (signal(SIGQUIT, sig_handler) == SIG_ERR) {
+        printf("\ncan't catch SIGINT\n");
+        return -1;
+    }
+
+    if (signal(SIGTERM, sig_handler) == SIG_ERR) {
+        printf("\ncan't catch SIGINT\n");
+        return -1;
+    }
+
+    while(1)
+    {
+        printf("Hell0\n");
+        sleep(1);
+    }
+
+    return 0;
+}
